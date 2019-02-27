a5b894923738334519ddc2c4a668eaf4573cf91a
bifeng <hubifengmail@163.com>
2019-02-28 07:03:04 +0800

    update

--- leetcode/Two Sum II - Input array is sorted-190227.py
+++ leetcode/Two Sum II - Input array is sorted-190227.py
@@ -1,8 +1,7 @@
 '''
 https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/
 1, hash map
-2,
-3,
+2, 双指针法 - 两头逼近
 '''
 from typing import *
 
@@ -22,17 +21,19 @@ def twoSum(nums: List[int], target: int) -> List[int]:
             sum_value[nums[j]] = j
 
 # second method
+# Runtime  40 ms
 def twoSum(nums: List[int], target: int) -> List[int]:
     """
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
     """
-    i = 0
-    sum_value = {}
-    for j in range(len(nums)):
-        if target - nums[j] in sum_value.keys():
-            return [sum_value[target - nums[j]] + 1, j + 1]
+    i, j = 0, len(nums)-1
+    while i < j:
+        if nums[i] + nums[j] == target:
+            return [i,j]
+        elif nums[i] + nums[j] < target:
+            i += 1
         else:
-            sum_value[nums[j]] = j
+            j -= 1
 
